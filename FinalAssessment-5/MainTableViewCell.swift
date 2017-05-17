2d070cec17ce7efffbd14277c079cb23dd6a1d97
楷岷 張 <MIN@Min-2.local>
2017-05-17 14:38:48 +0800

    CoreData建立

---
+++ FinalAssessment-5/MainTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainTableViewCell.swift
+//  FinalAssessment-5
+//
+//  Created by 楷岷 張 on 2017/5/17.
+//  Copyright © 2017年 楷岷 張. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
---
+++ FinalAssessment-5/MainTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewController.swift
+//  FinalAssessment-5
+//
+//  Created by 楷岷 張 on 2017/5/17.
+//  Copyright © 2017年 楷岷 張. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+}
