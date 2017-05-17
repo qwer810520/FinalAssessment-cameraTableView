f8d2179cbe6aa40b06cf8f807bc315750479a370
楷岷 張 <MIN@Min-2.local>
2017-05-17 20:57:03 +0800

    新增以及載入資料完成

---
+++ FinalAssessment-5/AddPhotoViewController.swift
@@ -0,0 +1,85 @@
+//
+//  AddPhotoViewController.swift
+//  FinalAssessment-5
+//
+//  Created by 楷岷 張 on 2017/5/17.
+//  Copyright © 2017年 楷岷 張. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var showCameraPhoto: UIImageView!
+    @IBOutlet weak var InputText: UITextField!
+    @IBOutlet weak var scrollViewSet: UIScrollView!
+    var addPhoto:PhotoMo?
+    var image:UIImage?
+    var saveSwitch = false
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .camera
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        }
+        
+        InputText.delegate = self
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if saveSwitch == true {
+            if let name = InputText.text {
+                if let photo = image {
+                    if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                        addPhoto = PhotoMo(context: appDelegate.persistentContainer.viewContext)
+                        addPhoto?.name = name
+                        if let imageData = UIImagePNGRepresentation(photo) {
+                            addPhoto?.photo = NSData(data: imageData)
+                        }
+                        appDelegate.saveContext()
+                    }
+                }
+            }
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == InputText {
+        scrollViewSet.setContentOffset(CGPoint(x: 0, y: 250), animated: true)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == InputText {
+            scrollViewSet.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == InputText {
+        textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let picture = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            showCameraPhoto.image = picture
+            image = picture
+            saveSwitch = true
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
--- FinalAssessment-5/MainTableViewController.swift
+++ FinalAssessment-5/MainTableViewController.swift
@@ -7,17 +7,99 @@
 //
 
 import UIKit
+import CoreData
 
-class MainTableViewController: UITableViewController {
+class MainTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    var photoArray:[PhotoMo] = []
+    var fetchResultController:NSFetchedResultsController<PhotoMo>!
+    
 
+    @IBAction func AddButton(_ sender: UIBarButtonItem) {
+        let addPhotoViewController = self.storyboard?.instantiateViewController(withIdentifier: "AddPhotoViewController")
+        navigationController?.pushViewController(addPhotoViewController!, animated: true)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadCoreData()
         
+    }
+    
+    func loadCoreData() {
+        let fetchRequest:NSFetchRequest<PhotoMo> = PhotoMo.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            let content = appDelegate.persistentContainer.viewContext
+            fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: content, sectionNameKeyPath: nil, cacheName: nil)
+            fetchResultController.delegate = self
+            
+            do {
+               try fetchResultController.performFetch()
+                if let fetchedObjects = fetchResultController.fetchedObjects {
+                    photoArray = fetchedObjects
+                }
+            } catch {
+                
+            }
+            
+        }
+    }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        case .update:
+            if let indexPath = indexPath {
+                tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        default:
+            tableView.reloadData()
+        }
+        
+        if let fetchedObject = controller.fetchedObjects {
+            photoArray = fetchedObject as! [PhotoMo]
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if photoArray.count != 0 {
+            return photoArray.count
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MainTableViewCell
+        if photoArray.count != 0 {
+            cell.photoImage.image = UIImage(data: photoArray[indexPath.row].photo! as Data)
+            cell.nameLabel.text = photoArray[indexPath.row].name
+        }
+        
+        return cell
     }
     
     
